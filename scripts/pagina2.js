efffd1b1a3386e237df7c52429f4c4f2d4dd81be
Cecília <cecbrennand@gmail.com>
2026-01-27 21:31:37 -0300

    adicionei cores aos botões certos e errados com js

--- scripts/pagina2.js
+++ scripts/pagina2.js
@@ -1,5 +1,57 @@
-function escolha() {
-    let resposta = document.getElementById('resposta1')
+let respondeu = false //porque ninguém respondeu ainda
 
-    resposta.style.background = 'green'
+function escolha1() {
+
+    if (respondeu == true) {
+        return
+    }
+
+    respondeu = true
+
+    let resposta1 = document.getElementById('r1')
+
+    resposta1.style.background = 'green'
+}
+
+function escolha2() {
+    if (respondeu == true) {
+        return
+    }
+
+    respondeu = true
+
+    let resposta1 = document.getElementById('r1')
+    let resposta2 = document.getElementById('r2')
+
+    resposta1.style.background = 'green'
+    resposta2.style.background = 'red'
+
+}
+
+function escolha3() {
+    if (respondeu == true) {
+        return
+    }
+
+    respondeu = true
+
+    let resposta1 = document.getElementById('r1')
+    let resposta3 = document.getElementById('r3')
+
+    resposta1.style.background = 'green'
+    resposta3.style.background = 'red'
+}
+
+function escolha4() {
+    if (respondeu == true) {
+        return
+    }
+
+    respondeu = true
+
+    let resposta1 = document.getElementById('r1')
+    let resposta4 = document.getElementById('r4')
+
+    resposta1.style.background = 'green'
+    resposta4.style.background = 'red'
 }
