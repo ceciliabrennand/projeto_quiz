d7953dfaeec9852fbc2c0674e492142c46e69cec
Cecília <cecbrennand@gmail.com>
2026-01-28 16:19:25 -0300

    exercitando if

--- scripts/pagina12.js
+++ scripts/pagina12.js
@@ -1,5 +1,16 @@
 let resultadoFinal = localStorage.getItem('resultado')
-
 let res = document.getElementById('resultado')
+let frase = document.getElementById('frasefinal')
 
 res.innerText = `${resultadoFinal}/10`
+
+if (resultadoFinal >= 8){
+    frase.innerText = 'Uau... Você me conhece muito bem!'
+} else if (resultadoFinal >= 5) {
+    frase.innerText = 'É... Tá na média, mas não tá me conhecendo muito não.'
+} else {
+    frase.innerText = 'Péssimo! Não me conhece nem um pouco.'
+}
+
+
+
